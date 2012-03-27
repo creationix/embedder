4cb4d77bfb488d75a23425468245484b2aa50173
Tim Caswell <tim@creationix.com>
2012-03-27 13:06:01 -0500

    Make browser safe

--- module.js
+++ module.js
@@ -10,7 +10,7 @@ define = function define(name, fn) {
   defs[name] = fn;
 }
 
-var realRequire = require;
+var realRequire = typeof require !== "undefined" && require;
 // The first time a module is used, it's description is executed and cached.
 require = function require(name) {
   if (modules.hasOwnProperty(name)) return modules[name];
@@ -21,7 +21,10 @@ require = function require(name) {
     fn(module, exports);
     return modules[name] = module.exports;
   }
-  return realRequire(name);
+  if (realRequire) {
+    return realRequire(name);
+  }
+  throw new Error("Can't find module " + name);
 }
 
 }());
